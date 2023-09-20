37469071501accd47f6e0f092f927056be399d75
unknown <alexdichemix@gmail.com>
2023-09-20 13:51:25 +0300

    dz1

--- src/main/java/step/learning/basics/FilesDemo.java
+++ src/main/java/step/learning/basics/FilesDemo.java
@@ -2,13 +2,57 @@ package step.learning.basics;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
 public class FilesDemo {
 
+public void run(){
+    File dir = new File("./");
+    // стврення new File НЕ впливає на файлову систему, це лише
+    // програмний об'єкт, який відповідає за зазначений шлях
+    if (dir.exists()) {
+        System.out.println("Path exists");
+    } else {
+        System.out.println("Path does not exist");
+    }
 
-    public void run(){
+    System.out.printf("Path is %s %n",
+            dir.isDirectory() ? "directory" : "file");
+
+    System.out.println(dir.getAbsolutePath());
+
+    System.out.println(String.format("%-6s %-17s %-8s %s", "Mode", "LastWriteTime", "Length", "Name"));
+    System.out.println(String.format("%-6s %-17s %-8s %s","----","-------------","------","----"));
+    for (File file : dir.listFiles()) {
+        PrintFileInfo(file);
+    }
+
+}
+    private void PrintFileInfo(File file){
+        System.out.println(
+                String.format("%-6s %-17s %-8s %s",
+                        getMode(file),
+                        getDate(file.lastModified()),
+                        getLength(file),
+                        file.getName()));
+    }
+    private String getMode(File file){
+        return file.isDirectory()?"d-----":"-a----";
+    }
+    private String getLength(File file){
+        return file.isFile()?String.format("%s",file.length()):"";
+    }
+    private String getDate(long milliseconds) {
+        Date date = new Date(milliseconds);
+        Locale.setDefault(Locale.US);
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
+        return dateFormat.format(date);
+    }
+    public void run3(){
         //зберігання данних у файлах
         String filename = "test.txt";
         //всі види роботи з данними у файлі - через Stream
